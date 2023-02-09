86ad651bd905a26243d64b8d4d43d06422d6d71a
Anderson Martins <andersonmg@gmail.com>
2023-02-09 17:34:06 -0300

    scenedetect config

--- handler.py
+++ handler.py
@@ -1,5 +1,24 @@
 import json
 
+from scenedetect import open_video, ContentDetector, SceneManager
+
+# process the file to split the scenes
+# https://scenedetect.com/projects/Manual/en/latest/api.html
+def get_scene_manager(video_path, threshold=27.0, min_scene_len=15) -> SceneManager:
+    video = open_video(video_path)
+    scene_manager = SceneManager()
+    scene_manager.add_detector(
+        ContentDetector(threshold=threshold, min_scene_len=min_scene_len))
+    # Detect all scenes in video from current position to end.
+    scene_manager.detect_scenes(video, show_progress=True)
+    # `get_scene_list` returns a list of start/end timecode pairs
+    # for each scene that was found.
+    return scene_manager  # .get_scene_list()
+
+
+def get_scene_list(video_path, threshold=27.0, min_scene_len=15) -> list:
+    return get_scene_manager(video_path, threshold, min_scene_len).get_scene_list()
+
 
 def scenes_extraction(event, context):
     body = {
